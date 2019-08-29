0717629bb9347370c1ae77fff8275a250876c21a
Lin Yuanhong <linyuanhong@tangyuandemacbook.local>
2019-08-29 21:36:06 +0800

    ss

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,14 @@
 module.exports = {
   siteMetadata: {
     title: 'NUM Design',
+  
     
-    // icon: '/Users/linyuanhong/Documents/nanmudesign/src/components/icon/numlogo2.png',
-    // link:{rel:'icon',type:'png',href:'/Users/linyuanhong/Documents/nanmudesign/src/components/icon/numlogo2.png' },
   },
 
 
   plugins: [
     'gatsby-plugin-react-helmet',
-    // `gatsby-plugin-sharp`,
+    
   
   ],
 }
