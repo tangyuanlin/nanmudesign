12d1b08a295a99f3078e88afb91da83bc8f5f3c5
Lin Yuanhong <linyuanhong@tangyuandemacbook.local>
2019-08-29 22:18:27 +0800

    77

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ const IndexPage = () => (
     <div className='bgc'></div>
     <h1 fontsize='1333px'>Welcome to NUM Design</h1>
     <p>Welcome to a new design team</p>
-    {/* <p>Now go see something great.</p> */}
+    <p>Now go see something great.</p>
  
     {/* <Link className="lk" to="/page-2/">Go to page 2</Link> */}
 
